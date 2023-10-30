fe076cf528024f71fe70f8b33477fa98218c42f8
Dario <106852390+Minkaspr@users.noreply.github.com>
2023-10-30 17:58:09 -0500

    modificacion de campos y metodos

--- index.php
+++ index.php
@@ -50,9 +50,11 @@ Flight::route('POST /pasajes', function () {
     $request = Flight::request();
 
     // Validar los datos del pasaje
-    $nombre = $request->data->nombre;
-    $apellido = $request->data->apellido;
-    $documentoIdentidad = $request->data->documentoIdentidad;
+    $primerNom = $request->data->primerNom;
+    $segundoNom = $request->data->segundoNom;
+    $apePaterno = $request->data->apePaterno;
+    $apeMaterno = $request->data->apeMaterno;
+    $numIdentidad = $request->data->numIdentidad;
     $telefono = $request->data->telefono;
     $origen = $request->data->origen;
     $destino = $request->data->destino;
@@ -61,21 +63,34 @@ Flight::route('POST /pasajes', function () {
     $precio = $request->data->precio;
 
     // Preparar la sentencia SQL
-    $sql = "INSERT INTO pasaje (nombre, apellido, documentoIdentidad, telefono, origen, destino, fecha, hora, precio) 
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    $sql = "INSERT INTO pasaje (primerNom, segundoNom, apePaterno, apeMaterno, numIdentidad, telefono, origen, destino, fecha, hora, precio) 
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
     try {
         // Ejecutar la sentencia SQL
         $sentencia = Flight::db()->prepare($sql);
-        $sentencia->bindParam(1,$nombre);
-        $sentencia->bindParam(2,$apellido);
-        $sentencia->bindParam(3,$documentoIdentidad);
-        $sentencia->bindParam(4,$telefono);
-        $sentencia->bindParam(5,$origen);
-        $sentencia->bindParam(6,$destino);
-        $sentencia->bindParam(7,$fecha);
-        $sentencia->bindParam(8,$hora);
-        $sentencia->bindParam(9,$precio);
+        $sentencia->bindParam(1,$primerNom);
+        // Si segundoNom es NULL, insertamos un NULL en la base de datos
+        if ($segundoNom != NULL) {
+            $sentencia->bindParam(2,$segundoNom);
+        } else {
+            $sentencia->bindValue(2,NULL, PDO::PARAM_NULL);
+        }
+        $sentencia->bindParam(3,$apePaterno);
+        $sentencia->bindParam(4,$apeMaterno);
+        $sentencia->bindParam(5,$numIdentidad);
+        // Si telefono es NULL, insertamos un NULL en la base de datos
+        if ($telefono != NULL) {
+            $sentencia->bindParam(6,$telefono);
+        } else {
+            $sentencia->bindValue(6,NULL, PDO::PARAM_NULL);
+        }
+        $sentencia->bindParam(7,$origen);
+        $sentencia->bindParam(8,$destino);
+        $sentencia->bindParam(9,$fecha);
+        $sentencia->bindParam(10,$hora);
+        $sentencia->bindParam(11,$precio);
+        // Ejecutamos la sentencia
         $sentencia->execute();
 
         Flight::json(["estado" => "exito", "mensaje" => "Pasaje agregado"], 201);
@@ -88,9 +103,11 @@ Flight::route('PUT /pasajes/@id', function ($id) {
     $request = Flight::request();
 
     // Validar los datos del pasaje
-    $nombre = $request->data->nombre;
-    $apellido = $request->data->apellido;
-    $documentoIdentidad = $request->data->documentoIdentidad;
+    $primerNom = $request->data->primerNom;
+    $segundoNom = $request->data->segundoNom;
+    $apePaterno = $request->data->apePaterno;
+    $apeMaterno = $request->data->apeMaterno;
+    $numIdentidad = $request->data->numIdentidad;
     $telefono = $request->data->telefono;
     $origen = $request->data->origen;
     $destino = $request->data->destino;
@@ -99,17 +116,30 @@ Flight::route('PUT /pasajes/@id', function ($id) {
     $precio = $request->data->precio;
 
     // Preparar la sentencia SQL
-    $sql = "UPDATE pasaje SET nombre = ?, apellido = ?, documentoIdentidad = ?, telefono = ?, origen = ?, destino = ?, 
+    $sql = "UPDATE pasaje SET primerNom = ?, segundoNom = ?, apePaterno = ?, apeMaterno = ?, numIdentidad = ?, telefono = ?, origen = ?, destino = ?, 
     fecha = ?, hora = ?, precio = ? WHERE id = ?";
 
     try {
         // Ejecutar la sentencia SQL
         $sentencia = Flight::db()->prepare($sql);
-        $sentencia->execute([$nombre, $apellido, $documentoIdentidad, $telefono, $origen, $destino, $fecha, $hora, $precio, $id]);
+        // Si segundoNom o telefono son NULL, insertamos un NULL en la base de datos
+        if ($segundoNom != NULL) {
+            $sentencia->bindParam(1,$segundoNom);
+        } else {
+            $sentencia->bindValue(1,NULL, PDO::PARAM_NULL);
+        }
+        if ($telefono != NULL) {
+            $sentencia->bindParam(5,$telefono);
+        } else {
+            $sentencia->bindValue(5,NULL, PDO::PARAM_NULL);
+        }
+        $sentencia->execute([$primerNom, $segundoNom, $apePaterno, $apeMaterno, $numIdentidad, $telefono, 
+                             $origen, $destino, $fecha, $hora, $precio, $id]);
 
         Flight::json(["estado" => "exito", "mensaje" => "Pasaje modificado"], 200);
     } catch (PDOException $e) {
-        Flight::json(['estado' => 'error', 'mensaje' => 'Hubo un error al modificar el pasaje: ' . $e->getMessage()], 500);
+        Flight::json(['estado' => 'error', 'mensaje' => 'Hubo un error al modificar el pasaje: ' . 
+                      $e->getMessage()], 500);
     }
 });
 
